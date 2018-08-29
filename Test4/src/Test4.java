7f5c6e51ac0d418cdcc4c22f37a83ce488183031
Hmachhuana <Hmachhuana@192.168.1.208>
2018-08-29 22:29:55 +0530

    WHILE LOOP

---
+++ Test4/src/Test4.java
@@ -0,0 +1,14 @@
+
+public class Test4 {
+
+	public static void main(String[] args) {
+	 int num=10,total=0,van=1;
+	 while(van<=num)
+		 {total=total+van;
+	 van++;}
+	 System.out.println("RESULT:"+total);
+	 
+
+	}
+
+}
