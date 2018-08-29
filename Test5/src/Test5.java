ab8eca9e5c92ac73c91110d15edef0f3741fbc2f
Hmachhuana <Hmachhuana@192.168.1.208>
2018-08-29 22:52:49 +0530

    FOR LOOP

---
+++ Test5/src/Test5.java
@@ -0,0 +1,14 @@
+
+public class Test5 {
+
+	public static void main(String[] args) {
+		int result=10,i,j=0;
+		for(i=1;i<=10;i++)
+		{
+			j=j+i;
+		}
+		System.out.println("RESULT: "+j);
+	}
+	
+}
+
